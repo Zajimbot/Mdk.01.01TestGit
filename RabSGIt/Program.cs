648bff35b98dd983194ca9c6cceb3c9cd81182e1
Artur <Karturokga@gmail.com>
2024-10-11 12:37:39 +0300

    Сложение двух чисел

---
+++ RabSGIt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabSGIt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+
+            a = Convert.ToInt32( Console.ReadLine());
+            b = Convert.ToInt32( Console.ReadLine());
+            c = a + b;
+            Console.WriteLine(c);
+
+        }
+    }
+}
